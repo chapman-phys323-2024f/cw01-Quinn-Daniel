13dec2335c75b0adcad16e02aeb082a5f0579660
Quinn Monahan <Qmonahan@chapman.edu>
2024-09-05 16:22:51 -0700

    I have fixed some typos

--- broken_script.py
+++ broken_script.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 # You will change this line later
-prnt("This script hs typs. Pleez fix them.")
+print("This script has typos. Pleez fix them.")
 # You will also change this line later
 
 print("Positive integers from 5 to 100:")
-for i in rnge(5,101)
+for i in range(5,101)
     print(i)
 
 print("This script is now fixed.")
