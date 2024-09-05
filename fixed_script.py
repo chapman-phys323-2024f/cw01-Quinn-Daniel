b5fa5f14d3f88c42fa4fd3702bea6fe9592f4e43
Daniel Yaacov <daniel.yaacov@outlook.com>
2024-09-05 16:27:48 -0700

    Fixed more typos, renamed file

---
+++ fixed_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# You will change this line later
+print("This script has typos. Please fix them.")
+# You will also change this line later
+
+print("Positive integers from 5 to 100:")
+for i in range(5,101):
+    print(i)
+
+print("This script is now fixed.")
